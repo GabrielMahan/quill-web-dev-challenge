02d05186cbf19dba16bd0759b4629881558a4aae
Gabriel Mahan <mahan.gabe@gmail.com>
2016-11-02 15:33:34 -0400

    pre componetization

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-// import logo from './logo.svg';
-// import {Editor, EditorState, RichUtils, ContentState, convertFromHTML, Draft} from 'draft-js';
 
 
 import React, { Component } from 'react';
@@ -10,20 +8,26 @@ import {Editor, EditorState, RichUtils, ContentState} from 'draft-js';
 class App extends Component {
   constructor() {
     super();
-    // debugger;
     this.state = {
-      editorState: EditorState.createWithContent(ContentState.createFromText(seedText)),
+      editorState: EditorState.createEmpty(),
       changes: [],
-      seedText: "please type text"
+      editMode: false
     };
-    this.onChange = this.onChange.bind(this)
+    this.onChange = this.onChange.bind(this);
     this.showChanges = this.showChanges.bind(this);
-    this.handleKeyCommand = this.handleKeyCommand.bind(this);
+    this.enableEdit = this.enableEdit.bind(this);
   }
 
   onChange(editorState) {
     this.showChanges();
     this.setState({editorState});
+  }
+
+  enableEdit() {
+    this.setState({
+      editMode: true,
+      editorState: EditorState.createWithContent(ContentState.createFromText(this.state.editorState.getCurrentContent().getPlainText()))
+    })
   }
 
   getChangeStartIdx(ref, curState) {
@@ -70,57 +74,32 @@ class App extends Component {
 
     return (
       <div className="App">
-        <button onClick={this._onBoldClick.bind(this)}>Make selected text Bold</button>
         <Editor
           editorState={editorState}
           onChange={this.onChange}
           handleKeyCommand={this.handleKeyCommand}
+          placeholder="Please write some text!"
           />
-          {this.state.changes[0] ?
-            <ul className="changeList">
-              {this.state.changes.map((changeLog, k) => {
-                return <li key={k}> "...{changeLog.changeSnippet}..."</li>
-              })}
-            </ul>
-          : null
+
+        {this.state.editMode ?
+          <div>
+            <span> Please make edits to your text! </span>
+            { this.state.changes[0] ?
+              <ul className="changeList">
+                {this.state.changes.map((changeLog, k) => {
+                  return <li key={k}> "...{changeLog.changeSnippet}..."</li>
+                })}
+              </ul>
+            : null
+            }
+          </div>
+        : <button className="next-button" onClick={this.enableEdit}> Next -> </button>
         }
+
       </div>
     );
   }
 }
 
-// <button className="showChanges-button" onClick={this.showChanges}> Show Changes </button>
-//   // {this.state.changes.map((log) => {
-  //   return <li> {log.changeSnippet} </li>
-  // })}
 
-  // handleKeyCommand(command) {
-  //   // console.log('COMMAND ===>>>>>> ', command)
-  //   const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
-  //   if (newState) {
-  //     // console.log('state handled')
-  //     this.onChange(newState);
-  //     return 'handled';
-  //   }
-  //   return 'not-handled';
-  // }
-  //
-  // _onBoldClick() {
-  //   this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
-
-
-  // debugger;
-  // console.log('plain text', this.state.editorState.getCurrentContent().getPlainText());
-  // console.log('last block text', this.state.editorState.getCurrentContent().getLastBlock().text);
-  // let ustack = this.state.editorState.getUndoStack()
-  // console.log('EDITOR STATE UNDO ', EditorState.undo(this.state.editorState))
-  // console.log('UNDO STACK ', this.state.editorState.getUndoStack());
-  // }
-
-  //  (editorState) => this.setState({editorState});
-
-
-  // let d = convertFromHTML(seedText)
-  // debugger;
-  // console.log('=========convertFromHTML=====',d)
 export default App;
