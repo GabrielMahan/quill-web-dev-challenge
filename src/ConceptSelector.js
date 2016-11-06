9ff72ed8f6acf6851eb897fc295543f44cd531ba
Gabriel Mahan <mahan.gabe@gmail.com>
2016-11-06 15:35:11 -0500

    removed this.state.changes altogether. Moving on to delete clean up code and export function

--- src/ConceptSelector.js
+++ src/ConceptSelector.js
@@ -5,10 +5,11 @@ class ConceptSelector extends React.Component {
   constructor() {
     super();
     this.state = {
-      changes: [],
+      // changes: [],
       diffData: [],
-      concepts: ['Not Sure', 'Punctuation', 'Commas','Capitalization', 'Sentence Clauses'],
-      data: {}
+      concepts: ['Not Sure', 'Articles', 'Commas', 'Proper Nouns'],
+      data: {},
+      compactedChangeList: []
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
@@ -18,7 +19,7 @@ class ConceptSelector extends React.Component {
   componentDidMount() {
     let diffData = this.props.diffData;
     let diffStack = this.props.diffData.filter((node) => !node.value.match(/^\s+$/));
-    debugger;
+    // debugger;
     // Loop: run through the stack,
     // push changes into a changeList arrray until you hit an unchanged element
     // When you hit that unchaned element, empty the changeList, and throw a bunch of info into a changeObject that goes into the changes.
@@ -33,10 +34,16 @@ class ConceptSelector extends React.Component {
       if (diffStack[0]) {
         if ((cur.added || cur.removed) && !diffStack[0].added && !diffStack[0].removed) {
           changeNodes.push(cur);
+          let headText = unchangedHead.value.substr(unchangedHead.value.length - 10); + " "
+          let tailText = " " + diffStack[0].value.substr(0,10);
+          let beforeText =  headText + changeNodes.filter( (n) => n.removed).map((m) => m.value ).join(' ') + tailText;
+          let afterText = headText + changeNodes.filter((n) => n.added ).map((m) => m.value).join(' ') + tailText;
           compactedChangeList.push({
             unchangedHead: unchangedHead,
             unchangedTail: diffStack[0],
-            changeNodes: changeNodes
+            changeNodes: changeNodes,
+            beforeText: beforeText,
+            afterText: afterText
           })
           unchangedHead = diffStack[0];
           changeNodes = [];
@@ -51,32 +58,30 @@ class ConceptSelector extends React.Component {
       else {
         if (cur.added || cur.removed) {
           changeNodes.push(cur);
+
+          let headText = unchangedHead.value.substr(unchangedHead.value.length - 10) + " ";
+          let beforeText =  headText + changeNodes.filter( (n) => n.removed).map((m) => m.value ).join(' ');
+          let afterText = headText + changeNodes.filter((n) => n.added ).map((m) => m.value).join(' ');
+
           compactedChangeList.push({
             unchangedHead: unchangedHead,
             unchangedTail: {value: ""},
-            changeNodes: changeNodes
+            changeNodes: changeNodes,
+            beforeText: beforeText,
+            afterText: afterText
           })
         }
       }
     }
 
-
-    debugger;
-
-
-    function wasChanged(value) {
-      return value.added || value.removed;
-    };
-
-    let changes = diffData.filter(wasChanged);
-    changes.forEach( (change) => {
-      this.setState({data: Object.defineProperty(this.state.data, change.value, {
+    compactedChangeList.forEach( (change) => {
+      this.setState({data: Object.defineProperty(this.state.data, change.changeNodes.map( (n) => n.value).join(), {
         value: this.state.concepts[0],
         configurable: true,
         writeable: true
       })})
     })
-    this.setState({changes: changes, diffData: diffData})
+    this.setState({diffData: diffData, compactedChangeList: compactedChangeList})
     // debugger;
 
   }
@@ -107,28 +112,32 @@ class ConceptSelector extends React.Component {
         </div>
 
 
-        { this.state.changes[0] ?
-          <div className="changeContainer">
-            {this.state.changes.map((change, j) => {
-               return (
-                 <div key={j} className="changeList">
-                   {change.added ? <span className="add tag"> Addition: </span> : <span className="del tag"> Deletion </span> }
-                   <span className="changeSnippet"> "{change.value}" </span>
-                   <select
-                     name={change.value}
-                     value={this.state.data[change]}
-                     onChange={this.handleChange}
-                     >
-                      { this.state.concepts.map((concept, k) => {
-                       return <option key={k} value={concept}> {concept} </option>
-                     })}
-                   </select>
-                 </div>
-              )
-            })}
-         </div>
-         : null
-         }
+
+         { this.state.compactedChangeList[0] ?
+           <div className="changeContainer">
+             {this.state.compactedChangeList.map((change, j) => {
+                return (
+                  <div key={j} className="changeList">
+                    <span className="before">BEFORE::: {change.beforeText}</span>
+                    <span className="after">AFTER::: {change.afterText}</span>
+
+
+
+                    <select
+                      name={change.changeNodes.map( (n) => n.value).join() }
+                      value={this.state.data[change]}
+                      onChange={this.handleChange}
+                      >
+                       { this.state.concepts.map((concept, k) => {
+                        return <option key={k} value={concept}> {concept} </option>
+                      })}
+                    </select>
+                  </div>
+               )
+             })}
+          </div>
+          : null
+          }
          <button className="next-button" type="submit" onClick={this.handleSubmit}> Submit Your Changes 	&#8594;</button>
       </div>
     )
@@ -144,3 +153,38 @@ export default ConceptSelector
 //   readOnly={false}
 //   customStyleMap={styleMap}
 //   />
+
+
+// debugger;
+
+//
+// function wasChanged(value) {
+//   return value.added || value.removed;
+// };
+//
+// let changes = diffData.filter(wasChanged);
+
+
+
+  // { this.state.changes[0] ?
+//   <div className="changeContainer">
+//     {this.state.changes.map((change, j) => {
+//       return (
+//         <div key={j} className="changeList">
+//           {change.added ? <span className="add tag"> Addition: </span> : <span className="del tag"> Deletion </span> }
+//           <span className="changeSnippet"> "{change.value}" </span>
+//           <select
+//             name={change.value}
+//             value={this.state.data[change]}
+//             onChange={this.handleChange}
+//             >
+//             { this.state.concepts.map((concept, k) => {
+//               return <option key={k} value={concept}> {concept} </option>
+//             })}
+//           </select>
+//         </div>
+//       )
+//     })}
+//   </div>
+//   : null
+// }
